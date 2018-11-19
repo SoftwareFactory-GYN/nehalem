4db632ffa6a6eddf1e0ecc9e6855ed997779261f
Nathan Martins <nathan.eua@gmail.com>
2018-11-19 20:40:56 -0200

    Addind middleware and server

--- web_app/main.go
+++ web_app/main.go
@@ -1,1 +1,25 @@
-package web_app
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middlewareOne")
+		next.ServeHTTP(w, r)
+		log.Println("Executing middlewareOne again")
+	})
+}
+func final(w http.ResponseWriter, r *http.Request) {
+	log.Println("Executing finalHandler")
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	finalHandler := http.HandlerFunc(final)
+
+	http.Handle("/", middlewareOne(finalHandler))
+	http.ListenAndServe(":3000", nil)
+}
