dd90a3fcc448f0a3e6a3fd2a36ae09550f3bee95
Nathan Martins <nathan.eua@gmail.com>
2018-11-20 13:29:24 -0200

    Mudando pra Dynamodb

--- rest_api/db/db.go
+++ rest_api/db/db.go
@@ -1,19 +1,48 @@
 package db
 
 import (
-	"github.com/gocql/gocql"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
-func GetSession() *gocql.Session {
-
-	var err error
-
-	cluster := gocql.NewCluster("localhost")
-	cluster.Keyspace = "nehalem"
-	Session, err := cluster.CreateSession()
+func GetSession() *dynamodb.DynamoDB {
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		log.Fatal("Error loading .env file")
 	}
 
-	return Session
+	creds := credentials.NewEnvCredentials()
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("sa-east-1"),
+		Credentials: creds,
+	})
+
+	// Create DynamoDB client
+	return dynamodb.New(sess)
+
+}
+
+func ListTables(svc *dynamodb.DynamoDB) {
+
+	result, err := svc.ListTables(&dynamodb.ListTablesInput{})
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Tables:")
+	fmt.Println("")
+
+	for _, n := range result.TableNames {
+		fmt.Println(*n)
+	}
+
 }
