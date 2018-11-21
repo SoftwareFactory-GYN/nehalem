48f11fa5e1eab151e6b83ae86781a762fb455abd
Nathan Martins <nathan.eua@gmail.com>
2018-11-20 23:59:17 -0200

    Serving html de login

--- web_app/main.go
+++ web_app/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"github.com/SoftwareFactory-GYN/nehalem/rest_api/middleware"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"net/http"
+	"os"
+	"strconv"
 )
 
-func middleware(next http.Handler) http.Handler {
+func authMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		auth := r.Header.Get("Authorization")
@@ -19,14 +24,32 @@ func middleware(next http.Handler) http.Handler {
 
 	})
 }
-func okView(w http.ResponseWriter, r *http.Request) {
-	log.Println("in view")
-	w.Write([]byte("OK"))
+
+func initRouter(r *mux.Router) {
+
+	// Endpoint used to serve login page
+	r.Handle("/login", LoginHandler).Methods("GET")
+
+	//Index endpoint
+	r.Handle("/", authMiddleware(middleware.JwtMiddleware.Handler(IndexHandler))).Methods("GET")
+
+	//Static
+	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+	r.PathPrefix("/static/").Handler(s)
+
 }
 
 func main() {
-	finalHandler := http.HandlerFunc(okView)
 
-	http.Handle("/", middleware(finalHandler))
-	http.ListenAndServe(":3001", nil)
+	port := 3001
+
+	r := mux.NewRouter()
+
+	// Add our routes
+	initRouter(r)
+
+	fmt.Println("Serving on: http://localhost:" + strconv.Itoa(port))
+
+	http.ListenAndServe(":"+strconv.Itoa(port), handlers.LoggingHandler(os.Stdout, r))
+
 }
